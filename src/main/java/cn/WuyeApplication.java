35c8015663f7578eab0c6a8fef03fd4ce67e3640
luopanfeng <luopanfeng1994@gmail.com>
2017-06-05 14:25:08 +0800

    修改

--- src/main/java/cn/WuyeApplication.java
+++ src/main/java/cn/WuyeApplication.java
@@ -3,13 +3,15 @@ package cn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class WuyeApplication {
+public class WuyeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WuyeApplication.class, args);
+		System.out.println("123");
 	}
 
 }
