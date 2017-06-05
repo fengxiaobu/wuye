b0e50f667afd888d43725736f6654b45b3457c4f
钟朝杰 <407119492@qq.com>
2017-06-05 15:51:49 +0800

    增加Company实体类,Mapper,Service及测试

--- src/main/java/cn/WuyeApplication.java
+++ src/main/java/cn/WuyeApplication.java
@@ -1,13 +1,16 @@
 package cn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
+@SpringBootApplication
 @PropertySource(value={"classpath:application.properties"})
 @ComponentScan(basePackages = "cn")
+@Configuration
 public class WuyeApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(WuyeApplication.class, args);
