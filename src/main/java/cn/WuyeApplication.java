0945809b777f1c7e64dd530fe82d9b0940584330
qjc <qinjuncai@irzhd.com>
2017-06-05 17:21:11 +0800

    处理ajax跨域问题

--- src/main/java/cn/WuyeApplication.java
+++ src/main/java/cn/WuyeApplication.java
@@ -1,11 +1,13 @@
 package cn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
+@SpringBootApplication
 @PropertySource(value={"classpath:application.properties"})
 @ComponentScan(basePackages = "cn")
 public class WuyeApplication extends SpringBootServletInitializer {
