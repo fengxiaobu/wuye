d5a657ed3e803cc9c4191b242f516d93037188cf
luopanfeng <luopanfeng1994@gmail.com>
2017-06-07 19:03:19 +0800

    更新启动文件

--- src/main/java/cn/WuyeApplication.java
+++ src/main/java/cn/WuyeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,11 +16,6 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource(value={"classpath:application.properties"})
 public class WuyeApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-
-		ConfigurableApplicationContext run = SpringApplication.run(WuyeApplication.class, args);
-		String[] beanNames =  run.getBeanDefinitionNames();
-
-
 		SpringApplication.run(WuyeApplication.class, args);
 	} 
 	@Override
