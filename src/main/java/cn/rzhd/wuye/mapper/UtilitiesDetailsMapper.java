1c5f26f475950ee12daed255c0a64fffcdd72757
钟朝杰 <407119492@qq.com>
2017-06-20 18:29:00 +0800

    修改BUG

--- src/main/java/cn/rzhd/wuye/mapper/UtilitiesDetailsMapper.java
+++ src/main/java/cn/rzhd/wuye/mapper/UtilitiesDetailsMapper.java
@@ -2,10 +2,11 @@ package cn.rzhd.wuye.mapper;
 
 import cn.rzhd.wuye.bean.UtilitiesDetails;
 import cn.rzhd.wuye.vo.query.UtilitiesQuery;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.math.BigDecimal;
 import java.util.List;
-
+@Mapper
 public interface UtilitiesDetailsMapper {
 
     int deleteByPrimaryKey(BigDecimal utilitiesDetailsId);
--- src/main/java/cn/rzhd/wuye/mapper/UtilitiesInvoiceMapper.java
+++ src/main/java/cn/rzhd/wuye/mapper/UtilitiesInvoiceMapper.java
@@ -1,10 +1,11 @@
 package cn.rzhd.wuye.mapper;
 
 import cn.rzhd.wuye.bean.UtilitiesInvoice;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.math.BigDecimal;
 import java.util.List;
-
+@Mapper
 public interface UtilitiesInvoiceMapper {
     int deleteByPrimaryKey(BigDecimal utilitiesInvoiceId);
 
