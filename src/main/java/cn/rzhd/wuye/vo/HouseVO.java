822a8fd9821687504fba945dbd3047b2a624930b
luopanfeng <luopanfeng1994@gmail.com>
2017-06-22 21:15:37 +0800

    房产状态

--- src/main/java/cn/rzhd/wuye/vo/HouseVO.java
+++ src/main/java/cn/rzhd/wuye/vo/HouseVO.java
@@ -51,8 +51,8 @@ public class HouseVO implements Serializable {
 	private String astrictStatus;// 限制缴费状态Y--启用,N--禁用
 	private Date updateDate;// 更新时间
 	private ProjectInfo projectInfo;//关联的项目信息
-	private String vdef8;// 备用字段8
-	private String vdef9;// 备用字段9
+	private String vdef8;// 备用字段8  入住申请状态
+	private String vdef9;// 备用字段9  装修申请状态
 	private String vdef10;// 备用字段10
 	
 	private ProjectVO projectVO;
